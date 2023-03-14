324e8af2742b72ffdf998d2e8015b41e783a6acb
ABHIJIT-MISHRA99 <mishraabhijit067@gmail.com>
2023-03-14 16:20:24 +0530

    changes added

--- src/main/java/com/abhi/FileScheduler/service/FileSchedulerServiceImpl.java
+++ src/main/java/com/abhi/FileScheduler/service/FileSchedulerServiceImpl.java
@@ -1,8 +1,10 @@
 package com.abhi.FileScheduler.service;
 
 import com.abhi.FileScheduler.dto.FileDTO;
+import com.abhi.FileScheduler.entity.FileConfig;
 import com.abhi.FileScheduler.mapper.FileMapper;
 import com.abhi.FileScheduler.repo.FileRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class FileSchedulerServiceImpl {
     @Autowired
     private FileMapper filemapper;
@@ -22,7 +25,7 @@ public class FileSchedulerServiceImpl {
     private String filename;
     private FileDTO fileDTO1;
     private int DayOfm;
-    private int DayOfweek;
+    private int DayOfWeek;
     private int month;
     private int minutes;
     private int hour;
@@ -31,63 +34,61 @@ public class FileSchedulerServiceImpl {
 
     @Scheduled(cron = "0 * * * * *")
     public void fetchFromfileConfig() throws FileNotFoundException {
-        List<FileDTO> fileDTOS;
-        if (fileDTO.getSchedule().equals("HOURLY")) {
-            fileDTOS = fileRepository.findAll().stream().map(a -> filemapper.convertFileConfigToFileDTO(a)).collect(Collectors.toList());
-            for (FileDTO fileDTO1 : fileDTOS) {
-                generatefileHourly();
+        List<FileConfig> fileDTOS;
+            fileDTOS = fileRepository.findAll();
+            for (FileConfig fileConfig : fileDTOS) {
+                if (fileConfig.getSchedule().equals("HOURLY")) {
+                    generatefileHourly();
+                    filename = fileConfig.getFileName();
+                    log.info(filename);
+                }
+                else if (fileConfig.getSchedule().equals("MONTHLY")){
+//                    DayOfm = fileConfig.getMonthly().getDayOfMonth();
+//                    month = fileConfig.getMonthly().getMonth();
+//                    minutes = fileConfig.getMonthly().getMinutes();
+                    hour = fileConfig.getMonthly().getHour();
+                    generatefileMonthly();
+                    filename = fileConfig.getFileName();
+                    log.info(filename);
+                }
+                else if (fileConfig.getSchedule().equals("WEEKLY")){
+//                    DayOfWeek = fileDTO1.getWeekly().getDayOfWeek();
+//                    hour = fileDTO1.getWeekly().getHour();
+//                    minutes = fileDTO1.getWeekly().getMinutes();
+                    generatefileWeekly();
+                    filename = fileConfig.getFileName();
+                    log.info(filename);
+                }
+                else if (fileConfig.getSchedule().equals("DAILY")){
+//                    Day = fileDTO1.getDaily().getDay();
+//                    hour = fileDTO1.getDaily().getHour();
+//                    minutes = fileDTO1.getDaily().getMinutes();
+                    generatefileDaily();
+                    filename = fileConfig.getFileName();
+                    log.info(filename);
+                }
+                else {
+                    throw new FileNotFoundException("Invalid file and was not found");
+                }
             }
-        } else if (fileDTO.getSchedule().equals("MONTHLY")) {
-            fileDTOS = fileRepository.findAll().stream().map(a -> filemapper.convertFileConfigToFileDTO(a)).collect(Collectors.toList());
-            for (FileDTO fileDTO1 : fileDTOS) {
-                DayOfm = fileDTO1.getMonthly().getDayOfMonth();
-                month = fileDTO1.getMonthly().getMonth();
-                minutes = fileDTO1.getMonthly().getMinutes();
-                hour = fileDTO1.getMonthly().getHour();
-                generatefileMonthly();
-            }
-        } else if (fileDTO.getSchedule().equals("WEEKLY")) {
-            fileDTOS = fileRepository.findAll().stream().map(a -> filemapper.convertFileConfigToFileDTO(a)).collect(Collectors.toList());
-            for (FileDTO fileDTO1 : fileDTOS) {
-                DayOfweek = fileDTO1.getWeekly().getDayOfWeek();
-                hour = fileDTO1.getWeekly().getHour();
-                minutes = fileDTO1.getWeekly().getMinutes();
-                generatefileWeekly();
-            }
-        } else if (fileDTO.getSchedule().equals("DAILY")) {
-            fileDTOS = fileRepository.findAll().stream().map(a -> filemapper.convertFileConfigToFileDTO(a)).collect(Collectors.toList());
-            for (FileDTO fileDTO1 : fileDTOS) {
-                Day = fileDTO1.getDaily().getDay();
-                hour = fileDTO1.getDaily().getHour();
-                minutes = fileDTO1.getDaily().getMinutes();
-                generatefileDaily();
-            }
-        } else {
-            throw new FileNotFoundException("Ivalid file and was not found");
-
         }
 
-    }
 
-    @Scheduled(cron = "0 minutes hour * * Day")
+
+    @Scheduled(cron = "0 * * * * *")
     private void generatefileDaily() {
-        filename = fileDTO1.getFileName();
-    }
 
-    @Scheduled(cron = "0 minutes hour * * DayOfweek")
+    }
+    @Scheduled(cron = "0 * * * * *")
     private void generatefileWeekly() {
-        filename = fileDTO1.getFileName();
     }
 
-    @Scheduled(cron = "0 minutes hour DayOfm month * ")
+    @Scheduled(cron = "0 * * * * *")
     private void generatefileMonthly() {
-        filename = fileDTO1.getFileName();
+
     }
-
-
-    @Scheduled(fixedRate = 60 * 60 * 1000)
+    @Scheduled(cron = "0 * * * * *")
     public void generatefileHourly() {
-        filename = fileDTO1.getFileName();
     }
 
 }
