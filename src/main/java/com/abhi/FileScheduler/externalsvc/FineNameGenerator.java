12218f46ef2eb68f0af3c8679f568b4c3ba6db9b
ABHIJIT-MISHRA99 <mishraabhijit067@gmail.com>
2023-03-19 18:26:37 +0530

    sendFileName code  modified

--- src/main/java/com/abhi/FileScheduler/externalsvc/FineNameGenerator.java
+++ src/main/java/com/abhi/FileScheduler/externalsvc/FineNameGenerator.java
@@ -3,16 +3,19 @@ package com.abhi.FileScheduler.externalsvc;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 @Component
 @Slf4j
 public class FineNameGenerator {
-
+    @Autowired
     private WebClient.Builder webClientBuilder;
+
     private URI uri;
     public FineNameGenerator() {
         uri= UriComponentsBuilder.fromHttpUrl("http://localhost:9009/file").build().toUri();
@@ -20,13 +23,20 @@ public class FineNameGenerator {
 
     public String sendFileName(String name) throws JsonProcessingException {
         WebClient webClient = webClientBuilder.build();
-
-        String block = webClient.post()
+        Mono<String> mono =webClient.post()
                 .bodyValue(new ObjectMapper().writeValueAsString(name))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        log.info("AccountSvcWebClient. NEW AC :" + block);
-        return block;
+                .exchangeToMono(
+                        response -> {
+                            if (response.statusCode().is2xxSuccessful()) {
+                                return response.bodyToMono(String.class);
+                            } else {
+                                return response.createException().flatMap(Mono::error);
+                            }
+                        }
+                );
+
+        String filename = mono.block();
+        log.info("AccountSvcWebClient. NEW AC :" + filename);
+        return filename;
     }
-}
+    }
